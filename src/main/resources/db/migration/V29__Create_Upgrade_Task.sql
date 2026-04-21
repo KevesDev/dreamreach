30dd09bcca18771726cc9ae18324554cfbe56aae
Riane <42573189+KevesDev@users.noreply.github.com>
2026-04-20 23:57:55 -0600

    Update V29__Create_Upgrade_Task.sql

--- src/main/resources/db/migration/V29__Create_Upgrade_Task.sql
+++ src/main/resources/db/migration/V29__Create_Upgrade_Task.sql
@@ -2,7 +2,7 @@
 CREATE TABLE upgrade_task (
                               id UUID PRIMARY KEY,
                               profile_id UUID NOT NULL REFERENCES player_profile(id),
-                              building_instance_id UUID NOT NULL REFERENCES building_instance(id),
+                              building_instance_id UUID NOT NULL REFERENCES building_instances(id),
                               target_level INT NOT NULL,
                               start_time TIMESTAMP WITH TIME ZONE NOT NULL,
                               completion_time TIMESTAMP WITH TIME ZONE NOT NULL
